b09390e3869e9d0cb98f3ea03d4cce5e3a955345
Nada Soliman <nsoliman@andrew.cmu.edu>
2023-03-04 21:40:25 +0300

    generated safety rankings for each data type

--- generate_safety_rankings.py
+++ generate_safety_rankings.py
@@ -2,12 +2,16 @@ import pandas as pd
 
 
 if __name__ == "__main__":
-    # TODO: currently generates ranking per trans, do for traj or not needed?
-    data_file = "old_generated_highway_data.csv"
+    # ! currently generates ranking for each trans, do for traj or not needed?
+    # ! why needed to stand/norm before ranking, ranking is unchanged
+    data_file = "normalized_standardized_data.csv"
     df = pd.read_csv(data_file)
 
-    df["safety_score"] = df[["num_collisions","num_offroad_visits"]].apply(tuple,axis=1)\
-                .rank(method='dense',ascending=False).astype(int)
+    for data_type in ["", "_normalized", "_standradized"]:
+        num_collisions = "num_collisions" + data_type
+        num_offroad_visits = "num_offroad_visits" + data_type
 
-    df.sort_values("safety_score")
-    df.to_csv("ranked_data_norm.csv")
+        df["safety_score" + data_type] = df[[num_collisions, num_offroad_visits]]\
+            .apply(tuple,axis=1).rank(method='dense',ascending=False).astype(int)
+
+    df.to_csv("data_safety_rankings.csv")
